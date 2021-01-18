7afa077ae23c3020950d604244f2d8c93eed155b
tetiana.molchanova-at-022587608743 <tetiana.molchanova@edetek.com>
2021-01-18 05:50:13 +0200

    todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { TodosFilterPipe } from './shared/todos-filter.pipe';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoFormComponent,
+    TodosFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { delay } from 'rxjs/operators';
+
+import { TodosService } from '../shared/todos.service';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+
+  public loading: boolean = true
+  private searchString: string = ''
+
+  constructor(public todosService: TodosService) { }
+
+  ngOnInit(): void {
+      this.todosService.fetchTodos()
+      .pipe(delay(500))
+      .subscribe(() => {
+        this.loading = false
+      })
+  }
+
+  onChange(id: number) {
+    this.todosService.onToggle(id)
+  }
+
+  removeTodo(id: number) {
+    this.todosService.removeTodo(id)
+  }
+
+}
